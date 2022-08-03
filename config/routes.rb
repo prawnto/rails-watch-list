61b4729772b3b07919ee1ed5d7bc5535cc047e88
Jie Lin Lee <lin.leejie@gmail.com>
2022-08-03 21:59:17 +0800

    replaced lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/create'
   get 'bookmarks/new'
   get 'bookmarks/create'
   get 'bookmarks/destroy'
